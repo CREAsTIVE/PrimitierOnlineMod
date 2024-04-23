e9084e290545c4a7ebf9830e1a898a2bcddd9c9d
YutoMaeda <UVTSAs@gmail.com>
2024-04-24 02:18:18 +0900

    Add Serilog

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System.Net;
 using System.Net.Sockets;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 class Settings {
     public string name { get; set; } = "";
@@ -12,9 +13,11 @@ class Settings {
     public int maxPlayer { get; set; } = 10;
 }
 
-[Serializable]
+[DataContract]
 class Command {
+    [DataMember]
     public string Name { get; set; } = "";
+    [DataMember]
     public object[] Args { get; set; } = new object[] {};
 
     public Command(string name, params object[] args) {
@@ -24,18 +27,23 @@ class Command {
 }
 
 class Program {
-    static IConfigurationRoot? config;
     static Settings? settings;
+    static Serilog.Core.Logger? logger;
     IPEndPoint[]? iPEndPoints;
     
     static void Main(string[] args) {
         try
         {
-            config = new ConfigurationBuilder()
+            IConfigurationRoot config = new ConfigurationBuilder()
                 .AddJsonFile("settings.json")
                 .Build();
             settings = new Settings();
             settings = config.Get<Settings>();
+
+            logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
             new Program().MainServer();
         }
         catch (Exception e)
@@ -49,19 +57,52 @@ class Program {
         iPEndPoints = new IPEndPoint[settings!.maxPlayer];
         TcpListener listener = new TcpListener(IPAddress.Any, settings.port);
 
-        listener.Start();
-        while (true)
+        logger!.Information("Server started on {0}:{1}", settings.ip, settings.port);
+        try
         {
-            ThreadPool.QueueUserWorkItem(TcpClient, listener.AcceptTcpClient());
+            listener.Start();
+            while (true)
+            {
+                ThreadPool.QueueUserWorkItem(TcpClient, listener.AcceptTcpClient());
+            }
+        } catch (Exception e)
+        {
+            ServerConsole.WriteLine(e.ToString());
         }
     }
 
     void TcpClient(object? obj) {
         try {
-            using (TcpClient client = (TcpClient)obj!) {
+            using (TcpClient client = (TcpClient)obj!)
+            using (NetworkStream stream = client.GetStream())
+            {
                 IPEndPoint remoteEndPoint = (IPEndPoint)client.Client.RemoteEndPoint!;
-                using (NetworkStream stream = client.GetStream()) {
-                    
+                byte[] buffer = new byte[4];
+                byte[] bytes;
+
+                stream.Read(buffer, 0, buffer.Length);
+                bytes = new byte[BitConverter.ToInt32(buffer, 0)];
+                stream.Read(bytes, 0, bytes.Length);
+
+                DataContractSerializer serializer = new DataContractSerializer(typeof(Command));
+                Command command = (Command)serializer.ReadObject(new MemoryStream(bytes))!;
+
+                if (command.Name == "connect")
+                {
+                    if (iPEndPoints!.Contains(remoteEndPoint)) {
+                        
+                    }
+
+                    foreach (IPEndPoint endPoint in iPEndPoints!)
+                    {
+                        if (endPoint == null)
+                        {
+
+                        }
+                    }
+                } else if (command.Name == "disconnect")
+                {
+
                 }
             }
         } catch (Exception e) {
@@ -74,10 +115,18 @@ class Program {
         UdpClient udpClient = new UdpClient();
         byte[] receivedData = udpClient.Receive(ref iPEndPoint);
         
-        foreach (IPEndPoint endPoint in iPEndPoints!) {
-            if (endPoint != null) {
-                udpClient.Send(receivedData, receivedData.Length, endPoint);
+        try
+        {
+            foreach (IPEndPoint endPoint in iPEndPoints!)
+            {
+                if (endPoint != null)
+                {
+                    udpClient.Send(receivedData, receivedData.Length, endPoint);
+                }
             }
+        } catch (Exception e)
+        {
+            ServerConsole.WriteLine(e.ToString());
         }
     }
 }
