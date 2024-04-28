07fa18fcb8ede9a7f2176c2659cc95e1c58bbe4b
YuchiGames <UVTSAs@gmail.com>
2024-04-28 15:23:41 +0900

    Update Data.cs

--- Server/Data.cs
+++ Server/Data.cs
@@ -1,6 +1,31 @@
 ﻿namespace Server.Data
 {
-    public class Plyaer {
+    public class Plyaer
+    {
+        public PosRot LeftHand { get; set; }
+        public PosRot RightHand { get; set; }
+
+        public Plyaer(PosRot leftHand, PosRot rightHand)
+        {
+            LeftHand = leftHand;
+            RightHand = rightHand;
+        }
+    }
+
+    public class Object
+    {
         
+    }
+
+    public class PosRot
+    {
+        public float[] Pos { get; set; }
+        public float[] Rot { get; set; }
+
+        public PosRot(float[] pos, float[] rot)
+        {
+            Pos = pos;
+            Rot = rot;
+        }
     }
 }
