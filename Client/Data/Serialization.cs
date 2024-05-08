8ae5e78cae62d3e6b0a025addab40a0dc9ee1a2e
YutoMaeda <UVTSAs@gmail.com>
2024-05-09 02:15:22 +0900

    Create client-side processing

---
+++ Client/Data/Serialization.cs
@@ -0,0 +1,38 @@
+﻿using MessagePack;
+using YuchiGames.POM.Client.Data.Methods;
+
+namespace YuchiGames.POM.Client.Data.Serialization
+{
+    public static class CommandsSerializer
+    {
+        public static object Deserialize(byte[] bytes)
+        {
+            try
+            {
+                switch (MessagePackSerializer.Deserialize<MethodsName>(bytes).Name)
+                {
+                    case "Error":
+                        return MessagePackSerializer.Deserialize<Error>(bytes);
+                    default:
+                        throw new Exception("Invalid command.");
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("An error occurred during deserialization.", e);
+            }
+        }
+
+        public static byte[] Serialize(object obj)
+        {
+            try
+            {
+                return MessagePackSerializer.Serialize(obj);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("An error occurred during serialization.", e);
+            }
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,32 @@
 ﻿using MelonLoader;
+using Microsoft.Extensions.Configuration;
+using YuchiGames.POM.Client.Data.Settings;
+using YuchiGames.POM.Client.Network;
 
 namespace YuchiGames.POM.Client
 {
     public class Program : MelonMod
     {
+        public static ClientSettings? settings;
+
         public override void OnInitializeMelon()
         {
-            MelonLogger.Msg("Hello, world!");
+            try
+            {
+                IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile("settings.json")
+                .Build();
+                settings = config.Get<ClientSettings>();
+
+                Thread tcpThread = new Thread(Tcp.Listener);
+                Thread udpThread = new Thread(Udp.Listener);
+                tcpThread.Start();
+                udpThread.Start();
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error(e.Message);
+            }
         }
 
         public override void OnFixedUpdate()
