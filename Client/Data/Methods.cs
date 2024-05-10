ab37d0b65e90bc6987b397fec13c153782aed66d
YutoMaeda <UVTSAs@gmail.com>
2024-05-11 00:57:31 +0900

    Update Methods.cs

--- Client/Data/Methods.cs
+++ Client/Data/Methods.cs
@@ -21,17 +21,11 @@ namespace YuchiGames.POM.Client.Data.Methods
         [Key(0)]
         public string Name { get; set; } = "Connect";
         [Key(1)]
-        public string UserID { get; set; }
-        [Key(2)]
-        public string UserName { get; set; }
-        [Key(3)]
         public string Version { get; set; }
 
         [SerializationConstructor]
-        public Connect(string userID, string userName, string version)
+        public Connect(string version)
         {
-            UserID = userID;
-            UserName = userName;
             Version = version;
         }
     }
@@ -44,15 +38,22 @@ namespace YuchiGames.POM.Client.Data.Methods
     }
 
     [MessagePackObject]
-    public class Error
+    public class Success
     {
         [Key(0)]
-        public string Name { get; set; } = "Error";
+        public string Name { get; set; } = "Success";
+    }
+
+    [MessagePackObject]
+    public class Failure
+    {
+        [Key(0)]
+        public string Name { get; set; } = "Failure";
         [Key(1)]
         public Exception ExceptionMessage { get; set; }
 
         [SerializationConstructor]
-        public Error(Exception exception)
+        public Failure(Exception exception)
         {
             ExceptionMessage = exception;
         }
