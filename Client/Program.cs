b5c4a3b0e3d757dc9e174c85cc6665e9c12dde0d
YuchiGames <UVTSAs@gmail.com>
2024-05-13 01:27:19 +0900

    Update Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,5 @@
 ﻿using MelonLoader;
 using Microsoft.Extensions.Configuration;
-using YuchiGames.POM.Client.Data.Methods;
-using YuchiGames.POM.Client.Data.Serialization;
 using YuchiGames.POM.Client.Data.Settings;
 using YuchiGames.POM.Client.Network;
 
@@ -24,9 +22,6 @@ namespace YuchiGames.POM.Client
                 Thread udpThread = new Thread(Udp.Listener);
                 tcpThread.Start();
                 udpThread.Start();
-
-                Connect connect = new Connect("1", "YuchiGames", "0.0.0");
-                Tcp.Sender(CommandsSerializer.Serialize(connect));
             }
             catch (Exception e)
             {
