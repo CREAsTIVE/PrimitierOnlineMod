c13d421d55e026db91a0afddeca01e84b3d9f34c
YuchiGames <UVTSAs@gmail.com>
2024-05-14 08:35:07 +0900

    Update DiscordWrapper.cs

--- Client/DiscordWrapper.cs
+++ Client/DiscordWrapper.cs
@@ -1,10 +1,64 @@
 ﻿namespace YuchiGames.POM.Client.DiscordWrapper
 {
-    public class DiscordHandler
+    public static class DiscordSetup
     {
+        public static Discord.Discord? discord;
+
         public static void Initialize()
         {
+            discord = new Discord.Discord(1239248231737856060, (ulong)Discord.CreateFlags.Default);
+        }
+    }
 
+    public static class DiscordActivity
+    {
+        static Discord.ActivityManager? activityManager;
+        public static void Initialize()
+        {
+            activityManager = DiscordSetup.discord!.GetActivityManager();
+        }
+
+        public static void UpdateActivity(string state, string details)
+        {
+            Discord.Activity activity = new Discord.Activity
+            {
+                State = state,
+                Details = details,
+                Type = Discord.ActivityType.Playing
+            };
+
+            activityManager!.UpdateActivity(activity, (result) =>
+            {
+                if (result == Discord.Result.Ok)
+                {
+                    Console.WriteLine("Activity updated.");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to update activity.");
+                }
+            });
+        }
+
+        public static void ClearActivity()
+        {
+            if (activityManager == null)
+            {
+                Console.WriteLine("Activity manager is null.");
+                return;
+            }
+
+            activityManager.ClearActivity((result) =>
+            {
+                if (result == Discord.Result.Ok)
+                {
+                    Console.WriteLine("Activity cleared.");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to clear activity.");
+                }
+            });
         }
     }
 }
