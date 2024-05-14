8fc791ff37fedf8775c22f56baa2316943b8256b
YutoMaeda <UVTSAs@gmail.com>
2024-05-15 01:14:46 +0900

    AAA

---
+++ Client/Data/Settings.cs
@@ -0,0 +1,14 @@
+﻿namespace YuchiGames.POM.Client.Data.Settings
+{
+    public class ClientSettings
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+
+        public ClientSettings(string ip, int port)
+        {
+            IP = ip;
+            Port = port;
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,19 +1,36 @@
 ﻿using MelonLoader;
+using Microsoft.Extensions.Configuration;
+using YuchiGames.POM.Client.Data.Settings;
 
 namespace YuchiGames.POM.Client
 {
     public class Program : MelonMod
     {
+        public static ClientSettings? settings;
+
         public override void OnInitializeMelon()
         {
             try
             {
-
+                IConfigurationRoot config = new ConfigurationBuilder()
+                    .AddJsonFile("settings.json")
+                    .Build();
+                settings = config.Get<ClientSettings>();
             }
             catch (Exception e)
             {
                 MelonLogger.Error(e.Message);
             }
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+
+        public override void OnApplicationQuit()
+        {
+            base.OnApplicationQuit();
         }
     }
 }
