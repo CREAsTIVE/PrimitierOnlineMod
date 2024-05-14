ce1d758adfb578855b51d0ae0862f98389e41adb
YuchiGames <UVTSAs@gmail.com>
2024-05-14 12:36:43 +0900

    Update DiscordWrapper.cs

--- Client/DiscordWrapper.cs
+++ Client/DiscordWrapper.cs
@@ -22,9 +22,34 @@
         {
             Discord.Activity activity = new Discord.Activity
             {
-                State = state,
-                Details = details,
-                Type = Discord.ActivityType.Playing
+                State = "In Play Mode",
+                Details = "Playing the Trumpet!",
+                Timestamps =
+                {
+                    Start = 5,
+                },
+                Assets =
+                {
+                    LargeImage = "foo largeImageKey", // Larger Image Asset Value
+                    LargeText = "foo largeImageText", // Large Image Tooltip
+                    SmallImage = "foo smallImageKey", // Small Image Asset Value
+                    SmallText = "foo smallImageText", // Small Image Tooltip
+                },
+                Party =
+                {
+                    Id = "foo partyID",
+                    Size = {
+                        CurrentSize = 1,
+                        MaxSize = 4,
+                    },
+                },
+                Secrets =
+                {
+                    Match = "foo matchSecret",
+                    Join = "foo joinSecret",
+                    Spectate = "foo spectateSecret",
+                },
+                Instance = true,
             };
 
             activityManager!.UpdateActivity(activity, (result) =>
