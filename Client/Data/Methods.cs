55e1a374246d6ca7a7eee958223bdd47baee5891
YuchiGames <UVTSAs@gmail.com>
2024-05-15 16:52:00 +0900

    AAA

---
+++ Client/Data/Methods.cs
@@ -0,0 +1,61 @@
+﻿using MessagePack;
+
+namespace YuchiGames.POM.Client.Data.Methods
+{
+    [MessagePackObject]
+    public class MethodsName
+    {
+        [Key(0)]
+        public string Name { get; }
+
+        [SerializationConstructor]
+        public MethodsName(string name)
+        {
+            Name = name;
+        }
+    }
+
+    [MessagePackObject]
+    public class ConnectMethod
+    {
+        [Key(0)]
+        public string Name { get; } = "ConnectMethod";
+        [Key(1)]
+        public string Version { get; set; }
+
+        [SerializationConstructor]
+        public ConnectMethod(string version)
+        {
+            Version = version;
+        }
+    }
+
+    [MessagePackObject]
+    public class DisconnectMethod
+    {
+        [Key(0)]
+        public string Name { get; } = "DisconnectMethod";
+    }
+
+    [MessagePackObject]
+    public class SuccessMethod
+    {
+        [Key(0)]
+        public string Name { get; } = "SuccessMethod";
+    }
+
+    [MessagePackObject]
+    public class FailureMethod
+    {
+        [Key(0)]
+        public string Name { get; } = "FailureMethod";
+        [Key(1)]
+        public Exception ExceptionMessage { get; set; }
+
+        [SerializationConstructor]
+        public FailureMethod(Exception exception)
+        {
+            ExceptionMessage = exception;
+        }
+    }
+}
---
+++ Client/Network/Listeners.cs
@@ -0,0 +1,76 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using MelonLoader;
+using YuchiGames.POM.Client;
+
+namespace YuchiGames.POM.Server.Network.Listeners
+{
+    public static class Tcp
+    {
+        public static void Listener()
+        {
+            if (Program.settings == null)
+            {
+                MelonLogger.Error("Settings is null.");
+                return;
+            }
+            
+            TcpListener listener = new TcpListener(IPAddress.Parse(Program.settings.IP), Program.settings.Port);
+
+            try
+            {
+                listener.Start();
+                MelonLogger.Msg($"Tcp server started on port {Program.settings.Port}.");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    // _ = Task.Run(() => Process.Tcp.Client(client));
+                }
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error(e.Message);
+            }
+            finally
+            {
+                listener.Stop();
+            }
+
+            MelonLogger.Msg($"Tcp server stopped on port {Program.settings.Port}.");
+        }
+    }
+
+    public static class Udp
+    {
+        public static async void Listener()
+        {
+            if (Program.settings == null)
+            {
+                MelonLogger.Error("Settings is null.");
+                return;
+            }
+
+            try
+            {
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(Program.settings.IP), Program.settings.Port);
+                using (UdpClient listener = new UdpClient(iPEndPoint))
+                {
+                    MelonLogger.Msg($"Udp server started on port {Program.settings.Port}.");
+
+                    while (true)
+                    {
+                        UdpReceiveResult result = await listener.ReceiveAsync();
+                        // _ = Task.Run(() => Process.Udp.Client(result));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error(e.Message);
+            }
+
+            MelonLogger.Msg($"Udp server stopped on port {Program.settings.Port}.");
+        }
+    }
+}
