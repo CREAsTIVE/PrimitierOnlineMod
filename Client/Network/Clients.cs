ba43c850bcdbad9b39e612475a3fadd4076ade82
YutoMaeda <UVTSAs@gmail.com>
2024-06-14 17:40:50 +0900

    asdfghj

---
+++ Client/Network/Clients.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+
+namespace YuchiGames.POM.Client.Network
+{
+    public class Clients
+    {
+        public void Udp(UdpReceiveResult result)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -33,7 +33,6 @@ namespace YuchiGames.POM.Client
                 s_endPoint = value;
             }
         }
-        private Transform[] _playerTransforms = new Transform[2];
 
         public override void OnInitializeMelon()
         {
@@ -46,25 +45,6 @@ namespace YuchiGames.POM.Client
 
                 if (s_settings is null)
                     throw new Exception("Settings not found.");
-
-                Listeners listeners = new Listeners();
-                Thread udpThread = new Thread(listeners.Udp);
-                udpThread.Start();
-
-                Senders senders = new Senders();
-                ITcpMessage receiveMessage = senders.Tcp(new ConnectMessage(s_settings.Version, s_settings.Name));
-
-                switch (receiveMessage)
-                {
-                    case SuccessConnectionMessage successConnectionMessage:
-                        LoggerInstance.Msg($"Success Connection: {successConnectionMessage.YourID}, {successConnectionMessage.IDList}");
-                        break;
-                    case FailureMessage failureMessage:
-                        LoggerInstance.Error(failureMessage.ToString());
-                        break;
-                    default:
-                        throw new Exception("Unknown message type.");
-                }
             }
             catch (Exception e)
             {
@@ -76,8 +56,7 @@ namespace YuchiGames.POM.Client
         {
             try
             {
-                _playerTransforms[0] = GameObject.Find("/Player/LeftHand").transform;
-                _playerTransforms[1] = GameObject.Find("/Player/RightHand").transform;
+
             }
             catch (Exception e)
             {
@@ -101,8 +80,7 @@ namespace YuchiGames.POM.Client
         {
             try
             {
-                //LoggerInstance.Msg("Player 1 position: {0}.", PlayerTransforms[0].position);
-                //LoggerInstance.Msg("Player 2 position: {0}.", PlayerTransforms[1].position);
+
             }
             catch (Exception e)
             {
@@ -114,20 +92,7 @@ namespace YuchiGames.POM.Client
         {
             try
             {
-                Senders senders = new Senders();
-                ITcpMessage receiveMessage = senders.Tcp(new DisconnectMessage());
 
-                switch (receiveMessage)
-                {
-                    case SuccessMessage successMessage:
-                        LoggerInstance.Msg("Success Disconnection.");
-                        break;
-                    case FailureMessage failureMessage:
-                        LoggerInstance.Error(failureMessage.ToString());
-                        break;
-                    default:
-                        throw new Exception("Unknown message type.");
-                }
             }
             catch (Exception e)
             {
