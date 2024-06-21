f7c1e5e1b90412a25457d533d18373c4cff672e1
YutoMaeda <UVTSAs@gmail.com>
2024-06-22 02:49:03 +0900

    Add patches

---
+++ Client/Patches/AvatarVisibilityButton.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+
+namespace YuchiGames.POM.Client.Patches
+{
+    [HarmonyPatch(typeof(AvatarVisibilityButton), nameof(AvatarVisibilityButton.SwitchState))]
+    class AvatarVisibilityButtonPatch
+    {
+        private static void Prefix()
+        {
+            Melon<Program>.Logger.Msg("AvatarVisibilityButton.SwitchState() called!");
+        }
+
+        private static void Postfix()
+        {
+            Melon<Program>.Logger.Msg("AvatarVisibilityButton.SwitchState() finished!");
+        }
+    }
+}
---
+++ Client/Patches/StartButton.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+
+namespace YuchiGames.POM.Client.Patches
+{
+    [HarmonyPatch(typeof(StartButton), nameof(StartButton.OnPress))]
+    class StartButtonPatch
+    {
+        private static void Prefix()
+        {
+            Melon<Program>.Logger.Msg("StartButton.OnPress() called!");
+        }
+
+        private static void Postfix()
+        {
+            Melon<Program>.Logger.Msg("StartButton.OnPress() finished!");
+        }
+    }
+}
