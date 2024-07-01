f27087fee4630361eaf8a9c3c9636038d066e8e6
YutoMaeda <UVTSAs@gmail.com>
2024-07-02 00:29:34 +0900

    Update Server.cs

--- Client/Network/Server.cs
+++ Client/Network/Server.cs
@@ -1,28 +1,136 @@
-﻿using System.Net;
+﻿using MelonLoader;
+using MessagePack;
+using System.Net;
+using System.Net.Sockets;
+using YuchiGames.POM.DataTypes;
 
 namespace YuchiGames.POM.Client.Network
 {
     public class Listener
     {
-        private IPEndPoint _ipEndPoint;
-        public IPEndPoint RemoteEndPoint
+        private IPEndPoint _localEndPoint;
+        public IPEndPoint LocalEndPoint
         {
             get
             {
-                return _ipEndPoint;
+                return _localEndPoint;
             }
         }
+        private TcpListener _tcpListener;
+        private UdpClient _udpListener;
+        private CancellationTokenSource _tcpCancelTokenSource;
+        private CancellationTokenSource _udpCancelTokenSource;
 
-        public event EventHandler OnConnect;
-        public event EventHandler OnDisconnect;
+        public event EventHandler OnJoined;
 
         public Listener(int port)
         {
-            _ipEndPoint = new IPEndPoint(IPAddress.Any, port);
+            _localEndPoint = new IPEndPoint(IPAddress.Any, port);
+            _tcpListener = new TcpListener(_localEndPoint);
+            _udpListener = new UdpClient(_localEndPoint);
+            _tcpCancelTokenSource = new CancellationTokenSource();
+            _udpCancelTokenSource = new CancellationTokenSource();
         }
         public Listener(string ip, int port)
         {
-            _ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            _localEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            _tcpListener = new TcpListener(_localEndPoint);
+            _udpListener = new UdpClient(_localEndPoint);
+            _tcpCancelTokenSource = new CancellationTokenSource();
+            _udpCancelTokenSource = new CancellationTokenSource();
+        }
+
+        public void Start()
+        {
+            try
+            {
+                _tcpListener.Start();
+                Thread tcpThread = new Thread(() => Tcp(_tcpCancelTokenSource.Token));
+                tcpThread.Start();
+
+                Thread udpThread = new Thread(() => Udp(_udpCancelTokenSource.Token));
+                udpThread.Start();
+            }
+            catch (Exception e)
+            {
+                Melon<Program>.Logger.Error(e.Message);
+            }
+        }
+        private void Tcp(CancellationToken token)
+        {
+            try
+            {
+                Melon<Program>.Logger.Msg($"Tcp server started on port {LocalEndPoint.Port}.");
+
+                while (!token.IsCancellationRequested)
+                {
+                    TcpClient client = _tcpListener.AcceptTcpClient();
+                    if (token.IsCancellationRequested)
+                        token.ThrowIfCancellationRequested();
+                    _ = Task.Run(() =>
+                    {
+                        using (client)
+                        using (NetworkStream stream = client.GetStream())
+                        {
+                            if (client.Client.RemoteEndPoint is null)
+                                throw new Exception("RemoteEndPoint not found.");
+                            IPEndPoint remoteEndPoint = (IPEndPoint)client.Client.RemoteEndPoint;
+
+                            byte[] buffer = new byte[1024];
+                            stream.Read(buffer, 0, buffer.Length);
+
+                            switch (MessagePackSerializer.Deserialize<ITcpMessage>(buffer))
+                            {
+                                default:
+                                    throw new Exception($"Received unknown message from {remoteEndPoint}.");
+                            }
+                        }
+                    });
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Melon<Program>.Logger.Msg($"Tcp server stopped on port {LocalEndPoint.Port}.");
+            }
+            catch (Exception e)
+            {
+                Melon<Program>.Logger.Error(e.Message);
+            }
+            finally
+            {
+                _tcpListener.Stop();
+                Melon<Program>.Logger.Msg($"Tcp server stopped on port {_localEndPoint.Port}.");
+            }
+        }
+        private async Task Udp(CancellationToken token)
+        {
+            try
+            {
+                Melon<Program>.Logger.Msg($"Udp server started on port {LocalEndPoint.Port}.");
+
+                while (!token.IsCancellationRequested)
+                {
+                    UdpReceiveResult result = await _udpListener.ReceiveAsync();
+                    if (token.IsCancellationRequested)
+                        break;
+                    _ = Task.Run(() =>
+                    {
+
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                Melon<Program>.Logger.Error(e.Message);
+            }
+
+            Melon<Program>.Logger.Msg($"Udp server stopped on port {LocalEndPoint.Port}.");
+        }
+
+        public void Stop()
+        {
+            _tcpCancelTokenSource.Cancel();
+            _udpCancelTokenSource.Cancel();
         }
     }
 }
