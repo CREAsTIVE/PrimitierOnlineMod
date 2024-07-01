97b3ef6f44e68b5e5e4f5e93b2c9a4ad97ac0f66
YutoMaeda <UVTSAs@gmail.com>
2024-07-01 14:47:28 +0900

    Create Server.cs

---
+++ Client/Network/Server.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace YuchiGames.POM.Client.Network
+{
+    public class Listener
+    {
+        private IPEndPoint _ipEndPoint;
+        public IPEndPoint RemoteEndPoint
+        {
+            get
+            {
+                return _ipEndPoint;
+            }
+        }
+
+        public event EventHandler OnConnect;
+        public event EventHandler OnDisconnect;
+
+        public Listener(int port)
+        {
+            _ipEndPoint = new IPEndPoint(IPAddress.Any, port);
+        }
+        public Listener(string ip, int port)
+        {
+            _ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+        }
+    }
+}
