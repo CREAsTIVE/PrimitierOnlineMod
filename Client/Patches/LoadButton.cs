23eed1551a65541285f51d12ec8dd9305958448b
YutoMaeda <UVTSAs@gmail.com>
2024-07-06 02:22:55 +0900

    Create LoadButton.cs

---
+++ Client/Patches/LoadButton.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using YuchiGames.POM.Client.Network;
+
+namespace YuchiGames.POM.Client.Patches
+{
+    [HarmonyPatch(typeof(LoadButton), nameof(LoadButton.OnPress))]
+    class LoadButton_OnPress
+    {
+        static bool Prefix()
+        {
+            Sender sender = new Sender(Program.Settings.IP, Program.Settings.SendPort);
+
+            return true;
+        }
+
+        static void Postfix()
+        {
+
+        }
+    }
+}
