1129f6f113994c3b0dd280fd9bb4b154159a8b0c
YutoMaeda <UVTSAs@gmail.com>
2024-07-07 01:40:02 +0900

    Add Version Tag

--- Client/DataTypes/Settings.cs
+++ Client/DataTypes/Settings.cs
@@ -3,15 +3,13 @@
     public class ClientSettings
     {
         public string Name { get; set; }
-        public string Version { get; set; }
         public string IP { get; set; }
         public int Port { get; set; }
         public int ListenPort { get; set; }
 
-        public ClientSettings(string name, string version, string ip, int port, int listenPort)
+        public ClientSettings(string name, string ip, int port, int listenPort)
         {
             Name = name;
-            Version = version;
             IP = ip;
             Port = port;
             ListenPort = listenPort;
--- Server/DataTypes/Settings.cs
+++ Server/DataTypes/Settings.cs
@@ -4,15 +4,13 @@
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Version { get; set; }
         public int Port { get; set; }
         public int MaxPlayer { get; set; }
 
-        public ServerSettings(string name, string description, string version, int port, int maxPlayer)
+        public ServerSettings(string name, string description, int port, int maxPlayer)
         {
             Name = name;
             Description = description;
-            Version = version;
             Port = port;
             MaxPlayer = maxPlayer;
         }
