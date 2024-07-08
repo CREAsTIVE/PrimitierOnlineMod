1c17144d5f701015749bde250248154959c0e72b
YutoMaeda <UVTSAs@gmail.com>
2024-07-08 23:36:45 +0900

    Change settings

--- Client/DataTypes/Settings.cs
+++ Client/DataTypes/Settings.cs
@@ -2,13 +2,11 @@
 {
     public class ClientSettings
     {
-        public string Name { get; set; }
         public string IP { get; set; }
         public int Port { get; set; }
 
-        public ClientSettings(string name, string ip, int port)
+        public ClientSettings(string ip, int port)
         {
-            Name = name;
             IP = ip;
             Port = port;
         }
--- Server/DataTypes/Settings.cs
+++ Server/DataTypes/Settings.cs
@@ -2,15 +2,11 @@
 {
     public class ServerSettings
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
         public int Port { get; set; }
         public int MaxPlayer { get; set; }
 
-        public ServerSettings(string name, string description, int port, int maxPlayer)
+        public ServerSettings(int port, int maxPlayer)
         {
-            Name = name;
-            Description = description;
             Port = port;
             MaxPlayer = maxPlayer;
         }
