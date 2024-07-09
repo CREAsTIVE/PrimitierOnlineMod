12ef2921a9fb243e639504502e5a08ff7a4a987a
YutoMaeda <UVTSAs@gmail.com>
2024-07-10 03:36:40 +0900

    Update AssemblyInfo.cs

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using MelonLoader;
 using YuchiGames.POM.Client;
 
-[assembly: MelonInfo(typeof(Program), "PrimitierOnlineMOD", "0.0.0", "YuchiGames")]
+[assembly: MelonInfo(typeof(Program), "PrimitierOnlineMOD", "0.1.0.0", "YuchiGames")]
 [assembly: MelonGame("PrimitierDev", "Primitier")]
