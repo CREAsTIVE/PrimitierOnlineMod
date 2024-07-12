90a89b45cbe34c648a66062b0a1f96cab74e03fa
YutoMaeda <UVTSAs@gmail.com>
2024-07-13 02:52:23 +0900

    When press a load button, connect to server

---
+++ Client/Patches/StartButton.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+
+namespace YuchiGames.POM.Client.Patches
+{
+    [HarmonyPatch(typeof(StartButton), nameof(StartButton.OnPress))]
+    class StartButton_OnPress
+    {
+        private static bool Prefix()
+        {
+            NetworkManager.Connect();
+            return true;
+        }
+
+        private static void Postfix() { }
+    }
+}
