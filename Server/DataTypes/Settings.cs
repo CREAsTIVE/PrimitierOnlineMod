6b3382ba05fb3676cccc1f7c93d2caffca25d5de
YutoMaeda <UVTSAs@gmail.com>
2024-07-20 00:48:45 +0900

    Update Settings.cs

--- Server/DataTypes/Settings.cs
+++ Server/DataTypes/Settings.cs
@@ -5,7 +5,7 @@
         public int Port { get; }
         public int MaxPlayers { get; }
         public int MaxDataSize { get; }
-        public int? Seed { get; }
+        public int Seed { get; }
 
         public ServerSettings(int port, int maxPlayers, int maxDataSize, int seed)
         {
