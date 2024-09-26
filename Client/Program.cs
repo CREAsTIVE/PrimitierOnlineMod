8b3882ac55eb69689bfc6ccd19a4075907c28fc7
CREAsTIVE <alexshveev@gmail.com>
2024-09-26 12:14:44 +0500

    Better config system + fix Now config loads/created with already existing Newtonsoft.json.dll. An error that appeared when trying to create a config when it was missing has been fixed Added UnityUtils class with methods, that allows to get even inactive objects Fixed error with trying to get DayNightCycle button

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,7 +8,6 @@ using YuchiGames.POM.Shared;
 using System.Runtime.Versioning;
 using YuchiGames.POM.Client.Assets;
 using YuchiGames.POM.Client.Managers;
-using Microsoft.Extensions.Configuration;
 
 namespace YuchiGames.POM.Client
 {
@@ -24,34 +23,16 @@ namespace YuchiGames.POM.Client
         {
             string settingsPath = $"{Directory.GetCurrentDirectory()}/Mods/settings.json";
 
-            if (!File.Exists(settingsPath))
-            {
-                FileStream stream = File.Create(settingsPath);
-                JsonSerializerOptions jsonOptions = new JsonSerializerOptions
-                {
-                    WriteIndented = true
-                };
-                string json = JsonSerializer.Serialize(
-                    Settings,
-                    jsonOptions);
-                stream.Write(Encoding.UTF8.GetBytes(json));
-            }
+            Settings = ClientSettings.LoadFromFileOrCreateDefault(settingsPath);
 
-            IConfigurationRoot config = new ConfigurationBuilder()
-                .AddJsonFile(settingsPath)
-                .Build();
+            Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString()
+                ?? throw new Exception("Version not found.");
 
-            Settings = config.Get<ClientSettings>()
-                ?? throw new Exception("Settings not found.");
-
-            Version = (Assembly.GetExecutingAssembly().GetName().Version
-                ?? throw new Exception("Version not found."))
-                .ToString();
-
-            using RegistryKey? key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\SQMClient")
+            using RegistryKey? key = 
+                Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\SQMClient")
                 ?? throw new Exception("SQMClient not found");
 
-            string machineID = (key.GetValue("MachineId")?.ToString())
+            string machineID = key.GetValue("MachineId")?.ToString()
                 ?? throw new Exception("MachineId not found");
 
             UserGUID = machineID.Trim('{', '}');
